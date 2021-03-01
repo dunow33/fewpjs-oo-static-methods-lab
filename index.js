22fc818076b588a2e36edd6a20c298bd22a79531
dunow <dunow33@hotmail.com>
2021-03-01 12:15:28 -0500

    commit tree e8ebc197aa4cf68df4e18e6c49ade34783b1386e

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let stringArray = string.split(' ');
+    let newArray = [];
+
+    newArray.push(this.capitalize(stringArray[0]));
+
+    for(let i = 1; i < stringArray.length; i++) {
+      switch(stringArray[i]){
+          case 'the':
+          case 'a':
+          case 'an':
+          case 'but':
+          case 'of':
+          case 'and':
+          case 'for':
+          case 'at':
+          case 'by':
+          case 'from':
+            newArray.push(stringArray[i]);
+            break;
+          default:
+            newArray.push(this.capitalize(stringArray[i]));
+            break;
+      }
+    }
+
+    return newArray.join(' ');
+  }
 }
